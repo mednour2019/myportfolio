aba9440eec79c43461dba8037da30a1d5ac81bc6
mednour2019 <medn392512@gmail.com>
2024-07-25 02:28:10 +0100

    ytyt

--- project-description/js/event.js
+++ project-description/js/event.js
@@ -92,6 +92,13 @@ document.addEventListener("DOMContentLoaded", function () {
       videoSrc: "video/covid-app.webm",
       githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
+    project10: {
+      title: "psychotherapy application",
+      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      description: "This is a detailed description of Project 2.",
+      videoSrc: "video/oddo.webm",
+      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+    },
   };
 
   if (projects[projectId]) {
