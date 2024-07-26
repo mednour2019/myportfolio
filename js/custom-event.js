ef08afb76e67b4301ed708b50c30f553bc416c9c
mednour2019 <medn392512@gmail.com>
2024-07-26 12:18:03 +0100

    drive

--- js/custom-event.js
+++ js/custom-event.js
@@ -3,6 +3,6 @@ function openNewWindow(projectId) {
 }
 function openPdfViewer() {
   // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
-  var pdfUrl = '../pdf-viewer/pdf-files/taches-technos.pdf';
+  var pdfUrl = 'https://drive.google.com/file/d/1-YLLpkD1XZYsHrPUpUQ2PjugmbDmZJXh/preview';
   window.open('pdf-viewer/pdf-project-description.html?file=' + encodeURIComponent(pdfUrl));
 }
--- project-description/js/event.js
+++ project-description/js/event.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const projects = {
     project1: {
       title: "Angular DataTable Implementation",
-      technologies: "Angular ,Angular Material ,Type Script , HTML , CSS ,Bootstrap ,RxJS",
+      technologies:
+        "Angular ,Angular Material ,Type Script , HTML , CSS ,Bootstrap ,RxJS",
       description:
         "In this technical test project, I implemented a dynamic and responsive data table using Angular, designed to manage employee data retrieved from an external API. The project showcases advanced table functionalities, including pagination, sorting, filtering, and CRUD (Create, Read, Update, Delete) operations, all handled on the frontend.",
-      videoSrc: "https://drive.google.com/file/d/1SVLCW8LEn6g3AzBg9pFenYHlbkttpMty/preview",
+      videoSrc:
+        "https://drive.google.com/file/d/1SVLCW8LEn6g3AzBg9pFenYHlbkttpMty/preview",
       githubLink: "https://github.com/mednour2019/AngularModuleTest.git",
       keyFeatures: [
         {
@@ -26,113 +28,147 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         {
           title: "sorting",
-          description: "Enabled sorting on all columns, allowing users to sort data in ascending or descending order",
+          description:
+            "Enabled sorting on all columns, allowing users to sort data in ascending or descending order",
         },
         {
           title: "Filtering",
-          description: "Provided a comprehensive filtering feature to search across all columns, making it easy to find specific records.",
+          description:
+            "Provided a comprehensive filtering feature to search across all columns, making it easy to find specific records.",
         },
-        { title: "CRUD Operations", description: "Added functionality to add and remove employees directly from the table on the frontend, providing a seamless user experience." },
+        {
+          title: "CRUD Operations",
+          description:
+            "Added functionality to add and remove employees directly from the table on the frontend, providing a seamless user experience.",
+        },
       ],
     },
     project2: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/pschoterapie.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/13MBAs9ZqScgba9Km3ZlpzFgKOcD-injP/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     // Add more projects as needed
     project3: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/udemy-students.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1bfgjA4MROwlx81mD5bBFyaJb6TTOa81p/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project4: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/medical-app.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1MI2mnB9wiuAdBZFWW9OrggwQUJ2XAw1v/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project5: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/democube.mp4",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1T0w5kwAc12YWU1IaOkdl4FAMsrnfpuOh/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project6: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/mdx.mp4",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/15L7GpwoOsXmea4n49E4TOZVVV1smR5CS/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project7: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/demoweb.mp4",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1CvrL_ZkW2eHticMD9S1U-H3EqZckq9-R/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project8: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/house-rent.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1cm9uGlYPSqD5W1OKuESgLhZq5PN1laVV/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project9: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/covid-app.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1zf7uXrtPFrbFr2jJjmS3gzc6--dOtXIS/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project10: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/oddo.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1Dr8GtRqQIjtbFhchMKxCB67q_VYz1PEs/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project11: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/reporting.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1kvsUQj3V6d-x7XztU-rO1BsfQeoOkbvO/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project12: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/eth.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1vP7gcX3KLhS7XIoTql2c86n25SjRy82j/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project13: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/fakerfood.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1VoeJAijYPUBYbU_54PO9nweTrmEcfGN_/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project14: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/demofinal.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1HQccNXkQFeh6P6iXuehiwk_haR4U4IDS/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
     project15: {
       title: "psychotherapy application",
-      technologies: " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
+      technologies:
+        " Angular ,HTML , CSS ,Kendo UI ,Type script ,java script ,jquery ,.net core web api ,Linq ,Entity framework (code first) ,sql server ,MVC, C#",
       description: "This is a detailed description of Project 2.",
-      videoSrc: "video/demo.webm",
-      githubLink: "https://github.com/mednour2019/psychotherapie-applicationn.git",
+      videoSrc: "https://drive.google.com/file/d/1EQ_56p41HTnX9U1LQWx8_FTDJ1Y799hG/preview",
+      githubLink:
+        "https://github.com/mednour2019/psychotherapie-applicationn.git",
     },
   };
 
@@ -145,8 +181,8 @@ document.addEventListener("DOMContentLoaded", function () {
       .getElementById("project-description")
       .querySelector("p").textContent = projects[projectId].description;
 
-   var iframe = document.getElementById('video-iframe');
-   iframe.src = projects[projectId].videoSrc;
+    var iframe = document.getElementById("video-iframe");
+    iframe.src = projects[projectId].videoSrc;
 
     document.getElementById("github-link").querySelector("a").href =
       projects[projectId].githubLink;
