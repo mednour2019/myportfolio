f10b81ad91acd8b806077475bd1ad20d0cd3e436
mednour2019 <medn392512@gmail.com>
2024-07-27 15:44:01 +0100

    jhksdds

--- gallery-viewer/js/event-gall.js
+++ gallery-viewer/js/event-gall.js
@@ -8,79 +8,158 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const projects = {
       project17: {
-          title: "Gallery Project",
-          technologies: "HTML, CSS, JavaScript, Bootstrap",
-          description: "This project showcases a dynamic image gallery with modal view functionality.",
-          githubLink: "https://github.com/your-username/gallery-project",
+          title: "SSIS Project: ETL and  Data Sources",
+          technologies: "ssis, sql server",
+          description: "A scholarship project, as demanded by professors, involves working with multiple external data sources such as MySQL and Excel. The project aims to merge these heterogeneous data sources and perform joins using SSIS, finally centralizing the unified data in SQL Server.",
+          githubLink: "https://github.com/mednour2019/ssisHetero.git",
           keyFeatures: [
-              { title: "Dynamic Loading", description: "Loads project details and images based on URL parameters." },
-              { title: "Modal Gallery", description: "Click on the main image to open a full-screen modal gallery." },
-              { title: "Responsive Design", description: "Adapts to various screen sizes for optimal viewing." }
+            {
+                "title": "Transform Customer Data",
+                "description": "Extract, clean customer data from various sources."
+              },
+              {
+                "title": "Transform Product Data",
+                "description": "Process and standardize product data, ensuring it is accurate and ready for analysis."
+              },
+              {
+                "title": "Conversion and Sort Data",
+                "description": "Convert data types as needed and sort the data to prepare"
+              },
+              {
+                "title": "Transformation of Sales Data",
+                "description": "Transform sales data to integrate it with other datasets."
+              }
           ],
           images: [
-              { src: '../gallery-viewer/images/project17/TransformationClient.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/TransformationProduit.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/TransformationTemps.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/TransformationVentes.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/Ventes.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/Temps.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/Produit.PNG', description: 'Description for image 1' },
-              { src: '../gallery-viewer/images/project17/Client.PNG', description: 'Description for image 1' },
+              { src: '../gallery-viewer/images/project17/TransformationClient.PNG', description: 'Transformation customer data' },
+              { src: '../gallery-viewer/images/project17/TransformationProduit.PNG', description: 'join product from various data source' },
+              { src: '../gallery-viewer/images/project17/TransformationTemps.PNG', description: 'conversion and sort data' },
+              { src: '../gallery-viewer/images/project17/TransformationVentes.PNG', description: 'transformation of sales data' },
+              { src: '../gallery-viewer/images/project17/Ventes.PNG', description: 'Result of data' },
+              { src: '../gallery-viewer/images/project17/Temps.PNG', description: 'divide date to month year via SSIS' },
+              { src: '../gallery-viewer/images/project17/Produit.PNG', description: 'Result of  product data' },
+              { src: '../gallery-viewer/images/project17/Client.PNG', description: 'Result of customer data' },
               // Add more image objects as needed
           ]
       },
       project21: {
-        title: "Gallery Project",
-        technologies: "HTML, CSS, JavaScript, Bootstrap",
-        description: "This project showcases a dynamic image gallery with modal view functionality.",
-        githubLink: "https://github.com/your-username/gallery-project",
+        title: "Text Summarization Using Natural Language Processing (NLP)",
+        technologies: "NLP,python,pandas",
+        description: "For a scholarship project, the goal is to develop a system that takes an input text and processes it using various Natural Language Processing (NLP) techniques such as stemming, lemmatization, and others. The system will then analyze the text to identify key phrases and assign weights to these phrases. Finally, it will generate a concise summary (resume) of the original text.",
+        githubLink: "https://github.com/mednour2019/taln-resume.git",
         keyFeatures: [
-            { title: "Dynamic Loading", description: "Loads project details and images based on URL parameters." },
-            { title: "Modal Gallery", description: "Click on the main image to open a full-screen modal gallery." },
-            { title: "Responsive Design", description: "Adapts to various screen sizes for optimal viewing." }
+            {
+                "title": "Reading File in Python",
+                "description": "Load and read the input text file using Python."
+              },
+              {
+                "title": "Regular Text Substitution",
+                "description": "Replace special characters in the text with spaces to standardize the input."
+              },
+              {
+                "title": "Split Sentences",
+                "description": "Divide the text into individual sentences for easier processing."
+              },
+              {
+                "title": "Tokenization of Words",
+                "description": "Break down sentences into individual words (tokens)."
+              },
+              {
+                "title": "Lemmatization of Words",
+                "description": "Convert words to their base or root form using lemmatization."
+              },
+              {
+                "title": "Frequency Calculation of Words",
+                "description": "Calculate the frequency of each word in the text."
+              },
+              {
+                "title": "Scoring of Each Word",
+                "description": "Assign a score to each word based on its frequency and importance."
+              },
+              {
+                "title": "Weight of Sentences",
+                "description": "Calculate the weight of each sentence based on the scores of the words it contains."
+              },
+              {
+                "title": "Identify Most Frequent Sentences",
+                "description": "Select the most important sentences based on their weights."
+              },
+              {
+                "title": "Final Result: Summary Text",
+                "description": "Generate the final summary (resume) of the text using the most important sentences."
+              }
         ],
         images: [
-            { src: '../gallery-viewer/images/project21/1.origintext.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/2.cleaniing.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/3.TransfSentBySpacy.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/4.SplitSentce.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/5.wordsspacy.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/6.lemma.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/7.frequencisWord.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/8.scoreeachword.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/10.heightsenteces.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/11.top5frequencies.PNG', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project21/12FinalResult.PNG', description: 'Description for image 1' },
+            { src: '../gallery-viewer/images/project21/1.origintext.PNG', description: 'Read text' },
+            { src: '../gallery-viewer/images/project21/2.cleaniing.PNG', description: 'regular expression' },
+            { src: '../gallery-viewer/images/project21/3.TransfSentBySpacy.PNG', description: 'Divide text in sentences' },
+            { src: '../gallery-viewer/images/project21/4.SplitSentce.PNG', description: 'split sentences' },
+            { src: '../gallery-viewer/images/project21/5.wordsspacy.PNG', description: 'split to words' },
+            { src: '../gallery-viewer/images/project21/6.lemma.PNG', description: 'lemmatization' },
+            { src: '../gallery-viewer/images/project21/7.frequencisWord.PNG', description: 'frequencies words' },
+            { src: '../gallery-viewer/images/project21/8.scoreeachword.PNG', description: 'score of each words' },
+            { src: '../gallery-viewer/images/project21/10.heightsenteces.PNG', description: 'weight of each sentence ' },
+            { src: '../gallery-viewer/images/project21/11.top5frequencies.PNG', description: 'most frequent sentences' },
+            { src: '../gallery-viewer/images/project21/12FinalResult.PNG', description: 'final result' },
 
             // Add more image objects as needed
         ]
     },
     project23: {
-        title: "Gallery Project",
-        technologies: "HTML, CSS, JavaScript, Bootstrap",
-        description: "This project showcases a dynamic image gallery with modal view functionality.",
-        githubLink: "https://github.com/your-username/gallery-project",
+        title: "Within-Pain: A Short Film",
+        technologies: "Imagination and Mental Visualization , Scripting , Search for Emotions",
+        description: "Within-Pain is a short film that revolves around the life of a person named Hedi, who is deeply affected by the mysterious loss of his father. The story is told through flashbacks, showcasing Hedi's time studying at the university and the subsequent impact on his life. The father, though never visualized in the film, is a constant presence through the narrative of absence and loss. The film delves into Hedi's suffering and the social challenges he faces, painting a poignant picture of grief and resilience",
+        githubLink: "https://github.com/mednour2019/within-pain-script-art.git",
         keyFeatures: [
-            { title: "Dynamic Loading", description: "Loads project details and images based on URL parameters." },
-            { title: "Modal Gallery", description: "Click on the main image to open a full-screen modal gallery." },
-            { title: "Responsive Design", description: "Adapts to various screen sizes for optimal viewing." }
+            {
+                "title": "Avoiding Social Interaction Period (2018)",
+                "description": "During 2018, I avoid talking to people during period  and prefer to be alone, focusing on scripting with deep emotional engagement."
+              },
+              {
+                "title": "Emotional and Affective Target Objectives",
+                "description": "Craft the film with a strong emotional and affective target, aiming to deeply resonate with the audience."
+              },
+              {
+                "title": "Design in Excel",
+                "description": "Plan and organize the film's structure and scenes using Excel for better clarity and management."
+              },
+              {
+                "title": "Presentation to Centers for Collaboration",
+                "description": "Present the project to various centers to seek collaboration and share the workload due to cost constraints."
+              },
+              {
+                "title": "Conception of a Numeric Application",
+                "description": "Develop a digital application to enhance the experience of managing and visualizing scenes and events."
+              },
+              {
+                "title": "Organizing a Cinema Club",
+                "description": "As the president of the ultra cinema club at the university, organize events and DJ sessions to raise funds for the film."
+              },
+              {
+                "title": "Event Management and Fundraising",
+                "description": "Despite some events not being successful, continue to develop an application to manage these activities more effectively."
+              },
+              {
+                "title": "Aim for the Best Short Film",
+                "description": "With persistent effort and development, aim to create the best short film ever when the time is right."
+              }
         ],
         images: [
-            { src: '../gallery-viewer/images/project23/1.jpg', description: 'Description for image 1' },
-            { src: '../gallery-viewer/images/project23/2.JPG', description: 'Description for image 2' },
-            { src: '../gallery-viewer/images/project23/3.JPG', description: 'Description for image 3' },
-            { src: '../gallery-viewer/images/project23/4.JPG', description: 'Description for image 4' },
-            { src: '../gallery-viewer/images/project23/5.jpg', description: 'Description for image 5' },
-            { src: '../gallery-viewer/images/project23/6.jpg', description: 'Description for image 6' },
-            { src: '../gallery-viewer/images/project23/7.jpg', description: 'Description for image 7' },
-            { src: '../gallery-viewer/images/project23/8.jpg', description: 'Description for image 8' },
-            { src: '../gallery-viewer/images/project23/9.jpg', description: 'Description for image 9' },
-            { src: '../gallery-viewer/images/project23/10.PNG', description: 'Description for image 10' },
-            { src: '../gallery-viewer/images/project23/11.PNG', description: 'Description for image 11' },
-            { src: '../gallery-viewer/images/project23/12.png', description: 'Description for image 12' },
-            { src: '../gallery-viewer/images/project23/44.jpg', description: 'Description for image 13' },
-            { src: '../gallery-viewer/images/project23/111.jpg', description: 'Description for image 14' },
-            { src: '../gallery-viewer/images/project23/5555.jpg', description: 'Description for image 15' },
+            { src: '../gallery-viewer/images/project23/1.jpg', description: 'Anarchy Art: Paper Scripting' },
+            { src: '../gallery-viewer/images/project23/2.JPG', description: 'Managing Scripts: Words, Scenes, and More' },
+            { src: '../gallery-viewer/images/project23/3.JPG', description: 'Managing scripts KPIs in Excel: Tracking and Analysis' },
+            { src: '../gallery-viewer/images/project23/4.JPG', description: 'identify scenes details in Excel' },
+            { src: '../gallery-viewer/images/project23/5.jpg', description: 'From Conception to Decision: Automated Script Management' },
+            { src: '../gallery-viewer/images/project23/6.jpg', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/7.jpg', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/8.jpg', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/9.jpg', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/10.PNG', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/11.PNG', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/12.png', description: 'moon-light' },
+            { src: '../gallery-viewer/images/project23/44.jpg', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/111.jpg', description: 'A Photographic Journey' },
+            { src: '../gallery-viewer/images/project23/5555.jpg', description: 'A Photographic Journey' },
 
             // Add more image objects as needed
         ]
--- pdf-viewer/js/event-pdf.js
+++ pdf-viewer/js/event-pdf.js
@@ -8,39 +8,110 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const projects = {
         project16: {
-            title: "PDF Project",
-            technologies: "PDF, JavaScript",
-            description: "This project showcases a PDF document with dynamic content loading.",
+            title: "Insurance Data Processing and Analysis",
+            technologies: "Excel, SSIS , Sql server , Power bi, DAX",
+            description: "A scholarship project that involves obtaining insurance data in CSV format from professors, processing it, and centralizing it in SQL Server. The project utilizes SSIS for data processing and flow management, and the final step is to visualize the data using Power BI for insightful analysis.",
             pdfSrc: "https://drive.google.com/file/d/1sQsu7RSjE9-s_iZ3QGZ9xqoFhgozenWg/preview", // Replace with your actual PDF file path
-            githubLink: "https://github.com/your-username/pdf-project",
+            githubLink: "https://github.com/mednour2019/BiInsurance.git",
             keyFeatures: [
-                { title: "Dynamic Loading", description: "Loads project details based on URL parameters." },
-                { title: "PDF Integration", description: "Seamlessly embeds PDF documents into the webpage." },
-                { title: "Responsive Design", description: "Adapts to various screen sizes for optimal viewing." }
+                {
+                    "title": "Describe Data Source",
+                    "description": "Detail the insurance data obtained in CSV format from professors."
+                  },
+                  {
+                    "title": "ETL Process with SSIS",
+                    "description": "Use SSIS to perform ETL (Extract, Transform, Load) and load the data into SQL Server, managing constraints and data flow."
+                  },
+                  {
+                    "title": "Identify Data Issues",
+                    "description": "Precisely identify and address data issues and inconsistencies during the ETL process."
+                  },
+                  {
+                    "title": "Connect Power BI and Write DAX Queries",
+                    "description": "Connect to Power BI and write DAX queries to correct and refine the data."
+                  },
+                  {
+                    "title": "Visualize Data in Power BI",
+                    "description": "Create insightful visualizations in Power BI to support decision-making processes, complete with detailed interpretations."
+                  }
             ]
         },
         project20: {
-            title: "PDF Project",
-            technologies: "PDF, JavaScript",
-            description: "This project showcases a PDF document with dynamic content loading.",
+            title: "Machine Learning Scraping and Sentiment Analysis of Breaking Bad Reviews",
+            technologies: "python , Django , ML , scrapping data, html ,css",
+            description: "For my exam project, I chose to scrape reviews and apply machine learning techniques. Specifically, I focused on Breaking Bad reviews. I scraped the data and processed it using Python to handle null values and duplicates. I then generated a machine learning model to achieve high accuracy. The project includes a detailed sentiment analysis based on this model to ensure precision. Additionally, I developed a Django application that allows users to enter reviews and predicts whether the sentiment is neutral, positive, or negative.",
             pdfSrc: "https://drive.google.com/file/d/132fIlK4UPp2DjSucoqCeY37EwkhtCDE8/preview", // Replace with your actual PDF file path
-            githubLink: "https://github.com/your-username/pdf-project",
+            githubLink: "https://github.com/mednour2019/IA-scrap-bb-dja.git",
             keyFeatures: [
-                { title: "Dynamic Loading", description: "Loads project details based on URL parameters." },
-                { title: "PDF Integration", description: "Seamlessly embeds PDF documents into the webpage." },
-                { title: "Responsive Design", description: "Adapts to various screen sizes for optimal viewing." }
+                {
+                    "title": "Scraping Data with BeautifulSoup and Selenium",
+                    "description": "Collect reviews from multiple sites using BeautifulSoup and Selenium."
+                  },
+                  {
+                    "title": "Unify Data in One Data Source via Python",
+                    "description": "Merge and clean the scraped data into a single, unified data source using Python."
+                  },
+                  {
+                    "title": "Machine Learning Process and Treatment",
+                    "description": "Process the data for machine learning, handling null values and duplicates."
+                  },
+                  {
+                    "title": "Apply Machine Learning Algorithms",
+                    "description": "Implement and train machine learning models to analyze sentiment with high accuracy."
+                  },
+                  {
+                    "title": "Download Blob Model for Prediction",
+                    "description": "Save the trained machine learning model as a blob for prediction purposes."
+                  },
+                  {
+                    "title": "Inject Model into Django Interface",
+                    "description": "Integrate the machine learning model into a Django application for real-time predictions."
+                  },
+                  {
+                    "title": "User Review Input and Prediction Display",
+                    "description": "Allow users to input reviews in the Django interface and display the predicted sentiment (neutral, positive, or negative)."
+                  }
             ]
         },
         project22: {
-            title: "PDF Project",
-            technologies: "PDF, JavaScript",
-            description: "This project showcases a PDF document with dynamic content loading.",
+            title: "Machine Learning Analysis of Netflix Data",
+            technologies: "python, ML, TKINTER",
+            description: "The project aims to analyze Netflix data to gain insights into different types of content, specifically focusing on distinguishing between films and TV shows. By applying machine learning techniques, the analysis will classify and provide knowledge about the nature of each entry, whether it is a film or a TV show. This will help in understanding patterns and trends within Netflix's content library.",
             pdfSrc: "https://drive.google.com/file/d/1qZ4QE_hDYsGFXRwbA0jlGGG6DOADUreU/preview", // Replace with your actual PDF file path
-            githubLink: "https://github.com/your-username/pdf-project",
+            githubLink: "https://github.com/mednour2019/netflix-machine-learning.git",
             keyFeatures: [
-                { title: "Dynamic Loading", description: "Loads project details based on URL parameters." },
-                { title: "PDF Integration", description: "Seamlessly embeds PDF documents into the webpage." },
-                { title: "Responsive Design", description: "Adapts to various screen sizes for optimal viewing." }
+                {
+                    "title": "Visualize Initial Data",
+                    "description": "Provide an initial visualization of the Netflix data to understand its structure and distribution."
+                  },
+                  {
+                    "title": "Identify Problems and Deficiencies",
+                    "description": "Detect and address any issues or deficiencies in the data, such as missing values or inconsistencies."
+                  },
+                  {
+                    "title": "Clean and Treat Data",
+                    "description": "Perform data cleaning and preprocessing to prepare it for analysis."
+                  },
+                  {
+                    "title": "Visualize and Analyze Data",
+                    "description": "Create visualizations to analyze the data and extract meaningful insights."
+                  },
+                  {
+                    "title": "Transform Categorical Columns",
+                    "description": "Convert categorical columns into a suitable format for machine learning algorithms."
+                  },
+                  {
+                    "title": "Apply Various Algorithms",
+                    "description": "Implement multiple machine learning algorithms to classify the data."
+                  },
+                  {
+                    "title": "Choose the Best Accuracy Model",
+                    "description": "Evaluate the performance of each algorithm and select the one with the highest accuracy."
+                  },
+                  {
+                    "title": "Visualize Results in Tkinter Desktop Application",
+                    "description": "Develop a Tkinter desktop application to visualize the entire process and results."
+                  }
             ]
         }
         // Add more PDF projects here as needed
