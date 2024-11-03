e36e796804902925b3eb9946768fb5b253856063
mednour2019 <medn392512@gmail.com>
2024-11-03 23:53:02 +0100

    commit of time_line task

--- js/custom-event.js
+++ js/custom-event.js
@@ -16,4 +16,7 @@ function openBlogCourse(projectId) {
   window.location.href = 'course-blog/course-blog.html?project=' + projectId;
 }
 
+function openTimeLinePage(projectId) {
+  window.location.href = 'time-line-page/time-line.html?project=' + projectId;
+}
 
---
+++ time-line-page/js/time-line.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function getQueryParam(param) {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get(param);
+    }
+
+    const projectId = getQueryParam("project");
+
+    const projects = {
+     
+    };
+
+   
+
+      
+  });
+
+  function openTimeLinePage(projectId) {
+    window.location.href = 'time-line-page/time-line.html?project=' + projectId;
+  }
+  
+  
+  
